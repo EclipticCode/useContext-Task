bb88b35f3eb041b4887f7a2cb4f9e0dc7acd7401
daylightcoderj6 <jananibharathi6@gmail.com>
2024-04-08 07:49:25 +0530

    created Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import Card from './components/Card'
+import img1 from './assets/IPhone_X.png'
+import img2 from './assets/samsung.jpeg'
+import img3 from './assets/Pixel8.png'
+import img4 from './assets/Vivo.jpeg'
+import img5 from './assets/Oneplus9.png'
 const App = () => {
+  const cardDatas = [
+    {id: 1 , img : img1 , title : "Iphone X" , description : "An apple phone which is nothing like apple" , price : "$899"  },
+    {id: 2 , img : img2 , title : "Samsung Galaxy" , description : "Samsung's new variant which goes beyond Galaxy to the Universe" , price : "$649"  },
+    {id: 3 , img : img3 , title : "Google Pixel 8" , description : "Google's new launch with all AI features included" , price : "$799"  },
+    {id: 4 , img : img4 , title : "Vivo F9" , description : "Samsung's new variant which goes beyond Galaxy to the Universe" , price : "$449" },
+    {id: 5 , img : img5 , title : "One Plus 9" , description : "The all new One plus 9 with fast charging" , price : "$449" }
+  ]
   return (
-    <div>App</div>
+    <div>
+        <div className="container">
+          <div className="row">
+          {cardDatas.map(data => (
+            <div className='col-sm-12 col-md-6 col-lg-4 col-xl-4 justify-content-center margin' key={data.id}>
+              <Card
+              {...data}
+              />
+            </div>
+          ))}
+          </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Card.css"
+
+
+const Card = ({img , title , description ,price}) => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                  <div className="card  align-items-center" style= {{width: "18rem"}}>
+                     <img src={img} className="card-img-top img-fluid" style = {{ width :"80%" , height : "220px"}}alt="..."/>
+                  <div className="card-body">
+                     <h5 className="card-title">{title}</h5>
+                     <p className="card-text">{description}</p>
+                  <div>
+                     <button type="button" className="btn btn-secondary justify-content-start">-</button> 
+                     <span>
+                     <button type="button" className="btn btn-secondary justify-content-end plusButton">+</button>
+                     </span>
+                     </div>
+ 
+                  </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
